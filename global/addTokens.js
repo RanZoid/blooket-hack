b3d9d175cbe2ed2bbd17390b873e7dc9579aa3ea
RanZoid <98929228+RanZoid@users.noreply.github.com>
2022-02-03 19:46:45 -0600

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -1,5 +1,5 @@
 async function getName() {
-    const response = await fetch('https://api.blooket.com/api/users/verify-token', {
+    const response = await fetch('https://api.blooket.com/api/users/verify-xp', {
         method: "GET",
         headers: {
             "accept": "application/json, text/plain, */*",
@@ -13,10 +13,10 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const xp = Number(prompt('How many xp do you want to add to your account? (300 daily)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 3000) {
+        alert('You can only add up to 3000 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
@@ -24,21 +24,4 @@ async function addCurrencies() {
         headers: {
             "referer": "https://www.blooket.com/",
             "content-type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-            addedTokens: tokens,
-            addedXp: 300,
-            name: await getName()
-        })
-    });
-
-    if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
-    } else {
-        alert('An error occured.');
-    };
-
-};
-
-addCurrencies();
+      
